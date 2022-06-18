e91f917176befb2959eb032252a26f3bec2b4495
unknown <renandias@gec.inatel.br>
2022-06-18 16:29:24 -0300

    Create table with courses

--- curso_project/curso-frontend/src/app/app-routing.module.ts
+++ curso_project/curso-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CursoComponent } from './curso/curso.component';
+import { CursoComponent } from './views/curso/curso.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: 'curso',
     component: CursoComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
--- curso_project/curso-frontend/src/app/app.module.ts
+++ curso_project/curso-frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursoComponent } from './curso/curso.component';
+import { CursoComponent } from './views/curso/curso.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursoComponent
+    CursoComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
